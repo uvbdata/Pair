a42cdb4755f2ce093af8e430e12791072b670f2f
Thorsten Boldin <uvbdata@gmail.com>
2016-11-04 09:55:22 -0600

    Build Storyboad Interface Design

---
+++ Pair/Pair/PairTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PairTableViewCell.swift
+//  Pair
+//
+//  Created by Thorsten Boldin on 04/11/2016.
+//  Copyright © 2016 Thorsten Boldin. All rights reserved.
+//
+
+import UIKit
+
+class PairTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
