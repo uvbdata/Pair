0870f257927f9af6c7270c3dbab0842d749b1bd5
Thorsten Boldin <uvbdata@gmail.com>
2016-11-04 09:15:34 -0600

    Initial commit for development with XC7 project
    Created new XCode Project SingleView with Unit and UI Test.

---
+++ Pair/Pair/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pair
+//
+//  Created by Thorsten Boldin on 04/11/2016.
+//  Copyright © 2016 Thorsten Boldin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
